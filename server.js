549202f8aa1374926af63b2a016a67264f78af8a
Quaad <ajwood1998@hotmail.co.uk>
2025-05-07 20:15:52 +0200

    added basic routing - moving onto express to refactor the routing logic to make it more condensed

--- server.js
+++ server.js
@@ -2,19 +2,33 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-    console.log(req.url, req.method);
 
     // set header content type
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
+    let path = './views/'
+    switch(req.url) {
+        case '/':
+            path += 'index.html';
+            break;
+        case '/about': 
+            path += 'about.html';
+            break;
+        case '/about-me': 
+            res.setHeader('Location', '/about');
+            res.statusCode = 301;
+            res.end();
+            break;
+        default: 
+            path += '404.html'
+            res.statusCode = 404;
+            break;
+    }
 
-    fs.readFile('./views/index.html', { encoding: 'utf8' }, (err, data) => {
+    fs.readFile(path, (err, data) => {
         if(err) {
-            console.log(err)
-            res.statusCode = 500;
             res.end();
         } else {
-            console.log(data)
             res.write(data)
             res.end();
         }
